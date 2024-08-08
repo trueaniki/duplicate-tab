3d0b0b681486e3c35907f21b9a00e947930d161f
Aniki <anikipelau@gmail.com>
2024-08-09 02:53:55 +0300

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -14,19 +14,8 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 function duplicateTab() {
-	const currentSelection = vscode.window.activeTextEditor?.document.getText() || '';
 	const currentFileName = vscode.window.activeTextEditor?.document.fileName;
-
-	var setting: vscode.Uri = vscode.Uri.parse('untitled:' + currentFileName + '-duplicated');
-	vscode.workspace.openTextDocument(setting).then((a: vscode.TextDocument) => {
-		vscode.window.showTextDocument(a, 1, false).then(e => {
-			e.edit(edit => {
-				edit.insert(new vscode.Position(0, 0), currentSelection);
-			});
-		});
-	}, (error: any) => {
-		vscode.window.showErrorMessage("Cannot duplicate tab")
-	});
+	vscode.window.showTextDocument(currentFileName, -2, false);
 }
 
 // This method is called when your extension is deactivated
